65cc3908f7c11a6431f31c26469ec104d56ee93c
guodong <gd@tongji.edu.cn>
2019-03-05 16:47:43 +0800

    Updates

---
+++ precache-manifest.8b997192339daa4e1c4ec47a4a5843ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "17052c58743c882d3d06",
+    "url": "/gitcourse/static/js/runtime~main.17052c58.js"
+  },
+  {
+    "revision": "5de74d3773b5eb8b0109",
+    "url": "/gitcourse/static/js/main.5de74d37.chunk.js"
+  },
+  {
+    "revision": "3f2650c56f969832ac14",
+    "url": "/gitcourse/static/js/2.3f2650c5.chunk.js"
+  },
+  {
+    "revision": "5de74d3773b5eb8b0109",
+    "url": "/gitcourse/static/css/main.7c40aa0d.chunk.css"
+  },
+  {
+    "revision": "3f2650c56f969832ac14",
+    "url": "/gitcourse/static/css/2.98633c37.chunk.css"
+  },
+  {
+    "revision": "0c424f36248b18977f9ffd78533b4a62",
+    "url": "/gitcourse/index.html"
+  }
+];
